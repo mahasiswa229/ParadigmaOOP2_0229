f7e1f9d82ef1f901525beae5a52e16eac9014f92
mahasiswa229 <161399711+mahasiswa229@users.noreply.github.com>
2024-05-28 17:00:00 +0800

    isi abstrak class

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -3,5 +3,21 @@ using namespace std;
 
 class AbstraksiKlas
 {
+    private: string x,y;
 
+    public:
+    //method untuk mengisi nilai
+    //private member
+    void setXY(string a, string b)
+    {
+        x = a;
+        y = b;
+    }
+    //menampilkan nilai
+    void display()
+    {
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl; 
+    }
 };
+
