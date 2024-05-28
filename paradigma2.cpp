d512a97a161c45317a0a3e905b2f97c324ca7906
mahasiswa229 <161399711+mahasiswa229@users.noreply.github.com>
2024-05-28 15:45:50 +0800

    ngisi class orang

--- paradigma2.cpp
+++ paradigma2.cpp
@@ -3,5 +3,12 @@ using namespace std;
 
 class orang
 {
-   
+    public:
+    int umur;
+
+    orang(int pUmur) :
+    umur(pUmur)
+    {
+        cout << "orang dibuat dengan umur" << "\n" << endl;
+    }
 };
