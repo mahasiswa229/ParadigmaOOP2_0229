301779393d2fecbbe45c08c82e44e3ae0000b1ff
mahasiswa229 <161399711+mahasiswa229@users.noreply.github.com>
2024-05-28 15:58:17 +0800

    isi class budi

--- paradigma2.cpp
+++ paradigma2.cpp
@@ -36,5 +36,11 @@ class pelajar : public orang
 
 class budi : public pekerja, public pelajar
 {
-    
+    public:
+    budi(int pUmur) :
+    pekerja(pUmur),
+    pelajar(pUmur)
+    {
+        cout << "budi dibuat\n" << endl;
+    }
 };
